4df86b169c5f6349afc497de8ee46071db60c2c6
Francis Bello <bellofrancis87@gmail.com>
2022-02-12 19:07:43 +0100

    responsiveness on the work so far

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+const mobileNav = document.querySelector(".mobile-nav");
+const menuIcon = document.querySelector(".menu-icon");
+const mobileMenuItems = document.querySelectorAll(".mobile-nav .menu-items li");
+
+menuIcon.addEventListener("click", () => {
+  mobileNav.classList.toggle("active");
+});
+mobileMenuItems.forEach((i) => {
+  i.addEventListener("click", () => {
+    mobileNav.classList.remove("active");
+  });
+});
+
 // fix nav starts
 const navBar = document.querySelector(".navbar");
 const navHeight = navBar.getBoundingClientRect().height;
